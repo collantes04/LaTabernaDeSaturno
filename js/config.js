c0de638bb2ddf827d830d3e93788ce0eb3b4c730
collantes04 <joseantonio.sanchez.collantes.alu@iesfernandoaguilar.es>
2025-05-07 09:47:48 +0200

    commit8

---
+++ js/config.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const race = document.getElementById('race');
+  
+    race.addEventListener('click', function () {
+      alert('¡Has hecho clic en el div!');
+    });
+  });
---
+++ js/gameObjects/races.js
@@ -0,0 +1,50 @@
+function createElf() {
+    
+}
+
+function createTiefling() {
+    
+}
+
+function createDrow() {
+    
+}
+
+function createHuman() {
+    
+}
+
+function creatGithyanki() {
+    
+}
+
+function createDwarf() {
+    
+}
+
+function createHalfElf() {
+    
+}
+
+function createHalfling() {
+    
+}
+
+function createGnome() {
+    
+}
+
+function createDragonborn() {
+    
+}
+
+function createHalforc() {
+    
+}
+
+function race(name, speed, racefeat[], subrace) {
+    this.name = name;
+    this.speed = speed;
+    this.racefeat[] = racefeat[];
+    this.subrace = subrace;
+}
