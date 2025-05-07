6a0bc90d17b948f016134f278fad9e306c417bb2
collantes04 <joseantonio.sanchez.collantes.alu@iesfernandoaguilar.es>
2025-05-07 12:14:53 +0200

    commit10

--- js/config.js
+++ js/config.js
@@ -1,21 +1,12 @@
-document.addEventListener('DOMContentLoaded', function () {
-    const race = document.getElementById('race');
-    const seccion2 = document.getElementById('seccion2');
-    const irASeccion2 = document.getElementById('irASeccion2');
-    const volverASeccion1 = document.getElementById('volverASeccion1');
+document.addEventListener('DOMContentLoaded', function() {
+    const botonRace = document.getElementById('mostrarRace');
+    const mainscreen = document.getElementById('mainscreen');
 
-    // Mostrar la sección 1 inicialmente
-    race.style.display = 'block';  // Esto hace visible la sección 1
+    botonRace.addEventListener('click', function() {
+        // Limpiar cualquier contenido anterior de mainscreen
+        mainscreen.innerHTML = '';
 
-    // Evento para ir a la sección 2
-    irASeccion2.addEventListener('click', function () {
-      seccion1.style.display = 'none';  // Ocultar la sección 1
-      seccion2.style.display = 'block'; // Mostrar la sección 2
+        mainscreen.style.backgroundColor = 'blue';
+
     });
-
-    // Evento para volver a la sección 1
-    volverASeccion1.addEventListener('click', function () {
-      seccion2.style.display = 'none';  // Ocultar la sección 2
-      seccion1.style.display = 'block'; // Mostrar la sección 1
-    });
-  });
+});
