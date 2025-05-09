2b4edbf14e222336ee480081dcab5603f195c8d2
alumno <alumno@pc05-t6.inf>
2025-05-09 10:14:02 +0200

    commit 15

--- js/config.js
+++ js/config.js
@@ -6,26 +6,33 @@ document.addEventListener('DOMContentLoaded', function() {
     const botonAbilities = document.getElementById('mostrarAbilities');
     const mainscreen = document.getElementById('mainscreen');
     const botonRaceBarbaro = document.getElementById('')
+    
 
     botonRace.addEventListener('click', function() {
         // Limpiar cualquier contenido anterior de mainscreen
         mainscreen.innerHTML = '';
 
-        mainscreen.style.backgroundColor = 'blue';
+        mainscreen.style.backgroundColor = 'orange';
 
-        document.getElementById("mainscreen").innerHTML = "Hola";
 
 
         const Races = ["Bárbaro", "Bardo", "Clérigo", "Druida", "Guerrero", "Monje", "Paladín", "Explorador", "Pícaro", "Hechicero", "Brujo", "Mago"];
-        const iconRaces = ["recursos/iconsClass/barbarian.png","recursos/iconsClass/bard.png","recursos/iconsClass/cleric.png","recursos/iconsClass/druid.png",
-            "recursos/iconsClass/figther.png","recursos/iconsClass/monk.png","recursos/iconsClass/paladin.png","recursos/iconsClass/ranger.png","recursos/iconsClass/rogue.png",
-            "recursos/iconsClass/sorcerer.png","recursos/iconsClass/warlock.png","recursos/iconsClass/wizard.png"
+        const iconRaces = ["../recursos/iconsClass/barbarian.png","../recursos/iconsClass/bard.png","../recursos/iconsClass/cleric.png","../recursos/iconsClass/druid.png",
+            "../recursos/iconsClass/figther.png","../recursos/iconsClass/monk.png","../recursos/iconsClass/paladin.png","../recursos/iconsClass/ranger.png","../recursos/iconsClass/rogue.png",
+            "../recursos/iconsClass/sorcerer.png","../recursos/iconsClass/warlock.png","../recursos/iconsClass/wizard.png"
         ];
-        for (int i=0;i<Races.length();i++) {
-            mainscreen.innerHTML += '<img src = >';
-            mainscreen.innerHTML += '<div id="casillaSelectorPersonaje">' + Races[i] + '</div>';
-
+        mainscreen.innerHTML = `<div id="grupoSelectorPersonaje">`
+        const grupoSelectorPersonaje = document.getElementById('grupoSelectorPersonaje');
+        for (let i = 0; i < Races.length; i++) {
+            grupoSelectorPersonaje.innerHTML += `
+                <div id="casillaSelectorPersonaje">
+                    <img src="${iconRaces[i]}" alt="${Races[i]}" />
+                    <div class="textoClases">${Races[i]}</div>
+                </div>
+            `;
         }
+        mainscreen.innerHTML += `</div>`
+
         
 
     });
