02fd1c7572d11af83257ceaa36bd51fb23a64708
collantes04 <joseantonio.sanchez.collantes.alu@iesfernandoaguilar.es>
2025-05-14 10:56:14 +0200

    a

--- js/gameObjects/Race.js
+++ js/gameObjects/Race.js
@@ -23,67 +23,67 @@ function createDrow() {
 }
 
 function createHalfElf() {
-    let elf = new Race("Half-Elf", 
-    /*Desc*/           "Curious, ambitious, and versatile, half-elves are welcome everywhere, but struggle without a community to call their own.",
-    /*Velocidad*/       0, 
-    /*Subrazas*/        null, 
-    /*Proficiencies*/   null);
+    let halfElf = new Race("Half-Elf", 
+    /*Desc*/               "Curious, ambitious, and versatile, half-elves are welcome everywhere, but struggle without a community to call their own.",
+    /*Velocidad*/           0, 
+    /*Subrazas*/            null, 
+    /*Proficiencies*/       null);
 }
 
 function createHalfOrc() {
-    let human = new Race("Half-Orc", 
-    /*Desc*/             "The most common face in Faerûn, humans are known for their tenacity, creativity, and endless capacity for growth.",
-    /*Velocidad*/         30, 
-    /*Subrazas*/          null, 
-    /*Proficiencies*/     null);
+    let halfOrc = new Race("Half-Orc", 
+    /*Desc*/               "Creatures of intense emotion, half-orcs are more inclined to act than contemplate - whether the rage burning their bodies compels them to fight, or the love filling their hearts inspires acts of incredible kindness.",
+    /*Velocidad*/           30, 
+    /*Subrazas*/            null, 
+    /*Proficiencies*/       null);
 }
 
 function createHalfling() {
-    let elf = new Race("Halfling", 
-    /*Desc*/           "With ethereal countenances and long lifespans, elves are at home with nature's power, flourishing in light and dark alike.",
-    /*Velocidad*/       0, 
-    /*Subrazas*/        null, 
-    /*Proficiencies*/   null);
+    let halfling = new Race("Halfling", 
+    /*Desc*/                "Small yet capable, halflings prefer the comforts of home and hearth - but their natural luck and dexterity makes them fine adventurers.",
+    /*Velocidad*/            25, 
+    /*Subrazas*/             null, 
+    /*Proficiencies*/        null);
 }
 
 function createDwarf() {
-    let human = new Race("Dwarf", 
+    let dwarf = new Race("Dwarf", 
     /*Desc*/             "The most common face in Faerûn, humans are known for their tenacity, creativity, and endless capacity for growth.",
-    /*Velocidad*/         30, 
+    /*Velocidad*/         25, 
     /*Subrazas*/          null, 
     /*Proficiencies*/     null);
 }
 
 function createGnome() {
-    let elf = new Race("Gnome", 
-    /*Desc*/           "With ethereal countenances and long lifespans, elves are at home with nature's power, flourishing in light and dark alike.",
-    /*Velocidad*/       0, 
+    let gnome = new Race("Gnome", 
+    /*Desc*/             "As durable and unyielding as their homes of stone, dwarves are some of the finest warriors, miners, and smiths of Faerûn.",
+    /*Velocidad*/         0, 
     /*Subrazas*/        null, 
     /*Proficiencies*/   null);
 }
 
 function createTiefling() {
-    let human = new Race("Tiefling", 
-    /*Desc*/             "The most common face in Faerûn, humans are known for their tenacity, creativity, and endless capacity for growth.",
-    /*Velocidad*/         30, 
-    /*Subrazas*/          null, 
-    /*Proficiencies*/     null);
+    let tiefling = new Race("Tiefling", 
+    /*Desc*/                "Descended from devils of the Nine Hells, tieflings face constant suspicion in Faerûn. Thankfully, their arcane abilities make them natural survivors.",
+    /*Velocidad*/            30, 
+    /*Subrazas*/             null, 
+    /*Proficiencies*/        null);
 }
 
 function createGithyanki() {
-    let elf = new Race("Githyanki", 
-    /*Desc*/           "With ethereal countenances and long lifespans, elves are at home with nature's power, flourishing in light and dark alike.",
-    /*Velocidad*/       0, 
-    /*Subrazas*/        null, 
-    /*Proficiencies*/   null);
+    let githyanki = new Race("Githyanki", 
+    /*Desc*/                 "With a ruthlessness borne from mind flayer enslavement, githyanki ride the Astral Sea atop red dragons, bringing their silver swords and psionic might to bear against any trace of the illithid menace.",
+    /*Velocidad*/             0, 
+    /*Subrazas*/              null, 
+    /*Proficiencies*/         null);
 }
 
 function createDragonborn() {
-    let human = new Race("Dragonborn", 
-    /*Desc*/             "The most common face in Faerûn, humans are known for their tenacity, creativity, and endless capacity for growth.",
-    /*Velocidad*/         30, 
-    /*Subrazas*/          null, 
-    /*Proficiencies*/     null);
+    let dragonborn = new Race("Dragonborn", 
+    /*Desc*/                  "A proud race that values clan and skills above all else. Once enslaved by dragons, they strive to be self-sufficient, not wanting to be beholden to anyone, not even the gods.",
+    /*Velocidad*/              30, 
+    /*Subrazas*/               null, 
+    /*Proficiencies*/          null);
 }
 
 function createRaza{
--- js/wikiEnemigos.js
+++ js/wikiEnemigos.js
@@ -6,11 +6,12 @@ function getId(elemento){
 document.addEventListener('DOMContentLoaded', function(){
 
     const botones = document.querySelectorAll('input[type="button"]');
-
+    const vectorMonstruos = [createOwlbear()]
     botones.forEach(function(boton){
         boton.addEventListener('click', function(){
             let id = getId(boton);
             
+            const objetoActual = vectorMonstruos[id];
         });
     });
 });
