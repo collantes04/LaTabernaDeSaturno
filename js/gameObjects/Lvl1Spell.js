600fe1f123f54ae72941cf6e315abb78f1462bbb
collantes04 <joseantonio.sanchez.collantes.alu@iesfernandoaguilar.es>
2025-05-21 12:29:09 +0200

    a

--- js/gameObjects/Lvl1Spell.js
+++ js/gameObjects/Lvl1Spell.js
@@ -1,3 +1,18 @@
+
+class Spell {
+    constructor(name, desc, img, dado, nivel) {
+        this.name = name;
+        this.desc = desc;
+        this.img = img;
+        this.dado = dado;
+        this.nivel = nivel;
+    }
+    
+    get name(){
+        return this.name;
+    }
+}
+
 const level = "1";
 
 
